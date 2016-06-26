8b1a269b373eaf04b74bc5eea6163ea75e3c5563
stele99 <epple@mast-computer.de>
2016-06-26 11:13:08 +0200

    upd

--- IRSender/module.php
+++ IRSender/module.php
@@ -6,7 +6,7 @@
 			//Never delete this line!
 			parent::Create();
 			
-			$this->RegisterPropertyString("Port", 0);
+			$this->RegisterPropertyString("PORT", 0);
 			$this->RegisterPropertyString("IP", "");
 		}
 	
@@ -14,6 +14,18 @@
 			//Never delete this line!
 			parent::ApplyChanges();
 		}
+    private function iSend($remote, $command, $rep){
+      $host = $this->ReadPropertyString("PORT");
+      $port = $this->ReadPropertyString("IP");
+      
+      $fp = pfsockopen( "tcp://$host", $port, $errno, $errstr );
+      $write = fwrite( $fp, "SEND_ONCE pioneer $cmd | $rep" );
+      fclose($fp);  
+    }
+    
+    public function SendCommand($remote, $command, $repeat = 2){
+           $this->iSend($remote,$command, $repeat);
+    }
 		
 	}
 
