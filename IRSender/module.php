10fd1d9318f0ce76486b6cd236ca3136b12f2e4e
stele99 <epple@mast-computer.de>
2016-06-26 12:07:59 +0200

    fix1

--- IRSender/module.php
+++ IRSender/module.php
@@ -6,11 +6,32 @@
 			//Never delete this line!
 			parent::Create();
 			
+      // Variablenprofile
+      if(!IPS_VariableProfileExists("SE_IRButtons")){
+        IPS_CreateVariableProfile("SE_IRButtons", 1);
+        IPS_SetVariableProfileAssociation("SE_IRButtons", 0, "Power", "", 0xFFFFFF);
+        IPS_SetVariableProfileAssociation("SE_IRButtons", 1, "Lauter", "", 0xFFFFFF);
+        IPS_SetVariableProfileAssociation("SE_IRButtons", 2, "Leiser", "", 0xFFFFFF);
+        IPS_SetVariableProfileAssociation("SE_IRButtons", 3, "Mute", "", 0xFFFFFF);
+      }
+      if(!IPS_VariableProfileExists("SE_IRInput")){
+        IPS_CreateVariableProfile("SE_IRInput", 1);
+        IPS_SetVariableProfileAssociation("SE_IRInput", 0, "CD", "", 0xFFFFFF);
+        IPS_SetVariableProfileAssociation("SE_IRInput", 1, "Tuner", "", 0xFFFFFF);
+        IPS_SetVariableProfileAssociation("SE_IRInput", 2, "Audio", "", 0xFFFFFF);
+        IPS_SetVariableProfileAssociation("SE_IRInput", 3, "Aux", "", 0xFFFFFF);
+        IPS_SetVariableProfileAssociation("SE_IRInput", 4, "Radio", "", 0xFFFFFF);
+      }      
+      $this->RegisterVariableInteger("BUTTONS", "Befehl", "SE_IRButtons");
+      $this->EnableAction("BUTTONS");
+
+      $this->RegisterVariableInteger("INPUT", "Input", "SE_IRInput");
+      $this->EnableAction("INPUT");
+      
 			$this->RegisterPropertyString("PORT", 0);
 			$this->RegisterPropertyString("IP", "");
       $this->RegisterPropertyString("REMOTE", "");       
-      $this->RegisterVariableBoolean("POWER", "Power", "~Switch");
-      $this->EnableAction("POWER");
+
       
 		}
 	
@@ -33,11 +54,30 @@
     }
     
     
-    public function RequestAction($Ident, $value) {        
+    public function RequestAction($Ident, $value) {   
+      	#Map
+      	 $MapInput[0] = "KEY_CD";
+      	 $MapInput[1] = "KEY_TUNER";
+      	 $MapInput[2] = "KEY_AUDIO";
+      	 $MapInput[3] = "KEY_AUX";
+      	 $MapInput[4] = "KEY_RADIO";
+      	 
+      	 $MapButton[0] = "KEY_POWER";
+      	 $MapButton[1] = "KEY_VOLUMEUP";
+      	 $MapButton[2] = "KEY_VOLUMEDOWN";
+      	 $MapButton[3] = "KEY_MUTE";     
+         
         switch($Ident) {
-            case "POWER":
-                          $this->iSend("KEY_POWER");
+            case "BUTTONS":
+                          $this->iSend($MapButtons[$value]);
                 break;
+            
+            case "INPUT":
+                          $this->iSend($MapInput[$value]);
+                break;
+
+
+
             default:
                 throw new Exception("Invalid Ident");
         }
